4630ec2f724fea816511d2f7263d567bde461957
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-04-13 13:41:53 -0700

    reset default port to 3000

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-var server = app.listen(process.env.PORT || 3001, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   console.log('Server is listening on port %s', server.address().port);
 });
