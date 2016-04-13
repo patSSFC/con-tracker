2532a0e816dd0d4b865a6246fdccfabad7e2d80e
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-04-13 13:40:45 -0700

    establish connection to db and return function that takes in query

--- db/db-config.js
+++ db/db-config.js
@@ -1,15 +1,21 @@
 var pg = require('pg');
 pg.defaults.ssl = true;
 
-var connect = function (callback) {
-  //pg.connect requires a fn callback(err, client)
-  pg.connect(process.env.DATABASE_URL, callback);
+var db = function () {
+  var connect = function () {
+    return function (query) {
+      pg.connect(process.env.DATABASE_URL, function (err, client) {
+        if (err) {return err;}
+
+        console.log('connected');
+        console.log('running query ' + query);
+      });
+    };
+  };
+
+  return {
+    connect: connect,
+  };
 };
 
-//we will need to build functions to query this database.
-//all querys should start with a call to connect
-var getPolitican = function (callback) {
-  //call connect() here
-};
-
-exports.connect = connect;
+module.exports = db();
--- server.js
+++ server.js
@@ -2,15 +2,13 @@ var express = require('express');
 var db = require('./db/db-config.js');
 var app = express();
 
-//For example only.
-db.connect(function (err, client) {
-  console.log('Client Connected');
-});
+// db.connect(db.getPolitician);
+var connection = db.connect();
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-var server = app.listen(process.env.PORT || 3000, function () {
+var server = app.listen(process.env.PORT || 3001, function () {
   console.log('Server is listening on port %s', server.address().port);
 });
