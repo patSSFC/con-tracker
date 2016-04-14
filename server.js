cc0913cda4ce3e505b3ec6f49978f6efe9af3cd1
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-04-14 00:14:57 -0500

    update server to accept rudimentary query params

--- server.js
+++ server.js
@@ -2,15 +2,26 @@ var express = require('express');
 var db = require('./db/db-config.js');
 var app = express();
 
-var connection = db.connect();
-var create_table = 'CREATE TABLE legislators (first_name varchar(160), last_name varchar(160))';
-var insert = "INSERT INTO legislators VALUES ('taylor', 'wirtz')";
-
-// connection.query(query, null);
-connection.query(insert);
-
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/database/', function (req, res) {
+  var table = 'politicians';
+  var q = req.query.query === undefined ? '*' : req.query.query;
+  console.log(req.query.query);
+  var query = 'SELECT ' + q +  ' FROM ' + table;
+
+  db.connect().query(query, function (err, data) {
+    if (err) {
+      console.log(JSON.stringify(err));
+      res.sendStatus(500);
+    }
+
+    data;
+    res.status(200).send(data);
+  });
+
 });
 
 var server = app.listen(process.env.PORT || 3000, function () {
