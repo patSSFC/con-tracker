f1274042d955c859e501c72d18a2136aedd0bfc3
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-04-14 00:21:53 -0500

    typo fix and readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# CONTRACKER
+
+## Interacting with cache database
+The cache database holds bulk data regarding politicians. This includes common bio information as well as ids for common databases.
+
+The database can be queried at the ```/database/``` route. By default all columns and records will be returned.
+
+Future support is planned for filtering columns and rows.
--- server.js
+++ server.js
@@ -2,27 +2,26 @@ var express = require('express');
 var db = require('./db/db-config.js');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/database/', function (req, res) {
+var queryDB = function (req, res) {
   var table = 'politicians';
   var q = req.query.query === undefined ? '*' : req.query.query;
-  console.log(req.query.query);
   var query = 'SELECT ' + q +  ' FROM ' + table;
 
   db.connect().query(query, function (err, data) {
     if (err) {
-      console.log(JSON.stringify(err));
+      console.log('ERROR:: ' + JSON.stringify(err));
       res.sendStatus(500);
     }
 
-    data;
     res.status(200).send(data);
   });
+};
 
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
+
+app.get('/database/', queryDB);
 
 var server = app.listen(process.env.PORT || 3000, function () {
   console.log('Server is listening on port %s', server.address().port);
