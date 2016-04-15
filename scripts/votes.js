02695523991530e61594497dd15ebc333f303fc6
Austin Arias <thenandback@gmail.com>
2016-04-14 20:44:50 -0700

    created helper function to populate each object in the bill array

--- scripts/votes.js
+++ scripts/votes.js
@@ -7,10 +7,17 @@
   var voteHistory = 'http://api.nytimes.com/svc/politics/v3/us/legislative/congress/members/' + member + '/votes.json?api-key=43115978a94dd9f66543e486c78f855d:11:74987712';
 
   var votesArray = ['S J RES 23', 'H R 1927', 'H R 2130', 'H R 1644'];
-
   var findBill = function (targetArray, targetBill) {
     return targetBill.question === 'On Passage' &&
            targetArray.includes(targetBill.bill.number);
+  };
+
+  var buildBill = function (proto) {
+    var voteInfo = {};
+    voteInfo.billID = proto.bill.number;
+    voteInfo.title = proto.bill.title;
+    voteInfo.position = proto.position;
+    return voteInfo;
   };
 
   repos.requestRepos = function (callback) {
@@ -22,11 +29,7 @@
         var storage = data.results[0].votes;
         for (var i = 0; i < storage.length; i++) {
           if (findBill(votesArray, storage[i])) {
-            var voteInfo = {};
-            voteInfo.billID = storage[i].bill.number;
-            voteInfo.title = storage[i].bill.title;
-            voteInfo.position = storage[i].position;
-            repos.all.push(voteInfo);
+            repos.all.push(buildBill(storage[i]));
           }
         }
       }
