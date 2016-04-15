cdc6564a1e6a010188bff10ab3fe21f5c8654cf7
Austin Arias <thenandback@gmail.com>
2016-04-14 20:54:31 -0700

    add comments

--- scripts/votes.js
+++ scripts/votes.js
@@ -4,15 +4,15 @@
   repos.all = [];
 
   var member = 'B000574';
-  var voteHistory = 'http://api.nytimes.com/svc/politics/v3/us/legislative/congress/members/' + member + '/votes.json?api-key=43115978a94dd9f66543e486c78f855d:11:74987712';
+  var voteHistory = 'http://api.nytimes.com/svc/politics/v3/us/legislative/congress/members/' + member + '/votes.json?api-key=43115978a94dd9f66543e486c78f855d:11:74987712'; // Returns members last 100 votes.
 
-  var votesArray = ['S J RES 23', 'H R 1927', 'H R 2130', 'H R 1644'];
+  var votesArray = ['S J RES 23', 'H R 1927', 'H R 2130', 'H R 1644']; // Specific bills we want to store
   var findBill = function (targetArray, targetBill) {
-    return targetBill.question === 'On Passage' &&
+    return targetBill.question === 'On Passage' && // 'On Passage' means they're voting to pass the bill.
            targetArray.includes(targetBill.bill.number);
   };
 
-  var buildBill = function (proto) {
+  var buildBill = function (proto) { // Takes a bill object and creates a new bill object with specific properties.
     var voteInfo = {};
     voteInfo.billID = proto.bill.number;
     voteInfo.title = proto.bill.title;
