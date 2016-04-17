5b73c8f9cc2656866b7a50e9f53cb6b6d2b6f5ca
Pat Yolian <pyolian@gmail.com>
2016-04-17 12:00:15 -0700

    move ajax calls to bioController and chart.js to bioViews

---
+++ scripts/bioController.js
@@ -0,0 +1,53 @@
+(function(module) {
+
+  Filing.filings = [];
+  Filing.totalPerQuarter = [];
+
+  function Filing(file) {
+    for (var prop in file) {
+      if(file.hasOwnProperty(prop)) {
+        this[prop] = file[prop];
+      }
+    }
+  }
+
+  Contributor.contributors = [];
+
+  function Contributor(contrib) {
+    for (var prop in contrib) {
+      if(contrib.hasOwnProperty(prop)) {
+        this[prop] = contrib[prop];
+      }
+    }
+  }
+
+
+  var filingsRequest = "http://realtime.influenceexplorer.com/api//new_filing/?format=json&page=1&page_size=10&candidate_id=P60007168&apikey=a59f2c8227c949fe90f7ccb1c0cba86f";
+  var contribRequest = "http://www.opensecrets.org/api/?method=candContrib&cid=N00007360&cycle=2016&apikey=0250595e734190ea51d62e7b981ea2d5&output=json";
+
+  Filing.getFilings = function (next) {
+    $.getJSON(filingsRequest, function(data) {
+      Filing.filings = data.results;
+    }).done(function(data) {
+      Filing.totalPerQuarter = data.results.map(function(r) {
+        return r.tot_raised;
+      });
+      next();
+    });
+  }
+
+  Contributor.getContributors = function (next) {
+    $.getJSON('/opensecrets/?method=candContrib&cid=N00007360&cycle=2016&output=json', function(data) {
+      // console.log(data.response.contributors.contributor[0]["@attributes"]);
+      Contributor.contributors = data.response.contributors.contributor;
+    }).done(function() {
+      console.log(Contributor.contributors);
+      // Contributor.contributors.forEach(function(c) {
+      //   console.log(c['@attributes'].total);
+      // })
+    });
+  }
+
+  Contributor.getContributors();
+  this.Filing = Filing;
+})(window);
--- scripts/bioView.js
+++ scripts/bioView.js
@@ -1,27 +1,7 @@
 (function(module) {
 
-  Filing.filings = [];
-  Filing.totalPerQuarter = [];
-
-  function Filing(file) {
-    for (var prop in file) {
-      if(file.hasOwnProperty(prop)) {
-        this[prop] = file[prop];
-      }
-    }
-  }
-
-  // var testRequest = "http://realtime.influenceexplorer.com/api//new_filing/?format=json&page=1&page_size=10&candidate_id=P60007168&apikey=a59f2c8227c949fe90f7ccb1c0cba86f";
-
-  $.getJSON(testRequest, function(data) {
-    Filing.filings = data.results;
-  }).done(function(data) {
-    Filing.totalPerQuarter = data.results.map(function(r) {
-      return r.tot_raised;
-    });
-
+function createDoughnut () {
     var ctx = $('#financial-canvas').get(0).getContext('2d');
-    console.log(Filing.filings);
     var data = [
       {
         value: Filing.totalPerQuarter[0]/1000000,
@@ -46,9 +26,8 @@
     var myDoughnutChart = new Chart(ctx).Doughnut(data,{
       maintainAspectRatio: true
     });
+}
 
-  });
-    // console.log(Filing.totalPerQuarter);
+Filing.getFilings(createDoughnut);
 
-  this.Filing = Filing;
 })(window);
