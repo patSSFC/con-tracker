38b23a19f33f3cdcab90eaf8ff1a24448f682d47
Matthew Smith <mattsmith203@hotmail.com>
2016-04-18 20:40:55 -0700

    moved all files to public file in prep for merge from dev

--- public/scripts/searchView.js
+++ public/scripts/searchView.js
@@ -1,6 +1,6 @@
 (function(module){
   var results = {};
-
+  var resultsName = [];
   results.all = [];
 
   results.requestRepos = function() {
@@ -19,5 +19,12 @@
       }
     });
   };
+  results.requestRepos();
+  results.compile = function(){
+    for (var i = 1; i < results.all.rows.length; i += 1) {
+      resultsName.push(results.all.rows[i].firstname + " " + results.all.rows[i].lastname)
+    };
+  };
   module.results = results;
+  module.resultsName = resultsName;
 })(window);
