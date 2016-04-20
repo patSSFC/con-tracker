b28ef06f1c8c9c87f12dcf185d744a9acb90c63a
Pat Yolian <pyolian@gmail.com>
2016-04-19 19:37:53 -0700

    remove comments and logs

--- public/scripts/financeController.js
+++ public/scripts/financeController.js
@@ -25,7 +25,6 @@
 
   Filing.getFilings = function (next) {
     $.getJSON('/sunlight_finance//new_filing/?format=json&page=1&page_size=10&candidate_id=P60007168&apikey=a59f2c8227c949fe90f7ccb1c0cba86f', function(data) {
-      // Filing.filings = data.results;
     }).done(function(data) {
       Filing.totalPerQuarter = data.results.map(function(r) {
         return r.tot_raised;
@@ -44,19 +43,16 @@
     $.getJSON('/opensecrets/?method=candContrib&cid=N00007360&cycle=2016&output=json', function(data) {
       Contributor.contributors = data.response.contributors.contributor;
     }).done(function(data, message, xhr) {
-      // console.log(Contributor.contributors);
       var contribs = Contributor.contributors;
       contribs = contribs.map(function(c) {
         return buildContributor(c['@attributes']);
       });
       Contributor.contributors = contribs;
-      console.log(Contributor.contributors);
       Contributor.toHtml();
     });
   };
 
   Contributor.getContributors();
-  // Contributor.toHtml();
   this.Filing = Filing;
   this.Contributor = Contributor;
 })(window);
--- public/scripts/financeView.js
+++ public/scripts/financeView.js
@@ -29,8 +29,6 @@
   }
   Contributor.toHtml = function() {
     var template = Handlebars.compile($('#top10template').text());
-    console.log(Contributor.contributors);
-    // Contributor.context = Contributor.contributors
     var context = {contributors: Contributor.contributors};
     $('.contributor-list').append(template(context));
   };
