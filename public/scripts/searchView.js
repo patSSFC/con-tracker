fd6396004faaeb3ac40d495153e4743a749ed245
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-04-19 18:44:00 -0700

    udpate jquery and map searched name to id

--- public/scripts/searchView.js
+++ public/scripts/searchView.js
@@ -1,42 +1,56 @@
-(function(module){
+(function (module) {
   var results = {};
   var availableTags = [];
   results.all = [];
 
-  results.requestRepos = function() {
+  var getPoliticId = function (name) {
+    var politicId = results.all.rows.filter(function (row) {
+      return row.firstname + ' ' + row.lastname === name;
+    }).map(function (row) {
+      return row.fec_id;
+    });
+
+    console.log(politicId);
+  };
+
+  results.requestRepos = function () {
     $.ajax({
       url: '/database/',
       type: 'GET',
 
-      success: function(data, message, xhr) {
+      success: function (data, message, xhr) {
         console.log(data);
         console.log(message);
         results.all = data;
         results.compile();
       },
-      error: function(data, status) {
+
+      error: function (data, status) {
         console.log(data);
         console.log(status);
-      }
+      },
     });
   };
-  results.requestRepos();
-  results.compile = function(){
-    $(results.all.rows).each(function(i){
-      availableTags.push(results.all.rows[i].firstname + " " + results.all.rows[i].lastname)
-    });
-  };
-  $(function() {
 
-      $( "#searchField" ).autocomplete({
-        source: availableTags
-      });
-      $('.searchBtn').on('click', function(){
-          var userInput = $('#searchField').val();
-          console.log(userInput);
-      });
+  results.requestRepos();
+  results.compile = function () {
+    $(results.all.rows).each(function (i) {
+      availableTags.push(results.all.rows[i].firstname + ' ' + results.all.rows[i].lastname);
+      getPoliticId(results.all.rows[i].firstname + ' ' + results.all.rows[i].lastname);
     });
+  };
+
+  $(function () {
+
+    $('#searchField').autocomplete({
+        source: availableTags,
+      });
+    $('.searchBtn').on('click', function () {
+      var userInput = $('#searchField').val();
+      console.log(userInput);
+    });
+  });
+
   module.results = results;
-  module.resultsName = resultsName;
   module.availableTags = availableTags;
 })(window);
