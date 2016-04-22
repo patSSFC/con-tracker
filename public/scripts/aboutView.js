93a4299edcfca352eaf8ff5e4ed46fca5749b100
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-04-21 19:18:18 -0700

    about view created and udpate voting view

--- public/scripts/aboutView.js
+++ public/scripts/aboutView.js
@@ -1,3 +1,29 @@
 (function(module){
-  
+  var about = {};
+
+  aboutView.getSections = function(path, callback){
+    $.getJSON(path, function(data){
+      if (data.sections){
+        about.sections = data.sections;
+      }
+      callback(about.sections);
+    }).error(function(err){
+      console.log('There was an error getting about JSON');
+      callback(null);
+    });
+  };
+
+  aboutView.renderData = function(data){
+    var ctx = { sections: data };
+    var template = (Handlebars.compile$('#aboutTemplate').text());
+
+    $('#about-view').empty().append(template(ctx));
+  };
+
+  aboutView.index = function() {
+    about.getSections(about.renderData);
+  };
+
+  module.aboutView = aboutView;
+
 })(window);
--- public/scripts/routes.js
+++ public/scripts/routes.js
@@ -1,6 +1,6 @@
 // page('/', app.initIndexPage);
 console.log('LOADING ROUTES');
 page('/', results.compile);
-page('/about', aboutView.about);
+page('/about', aboutView.index);
 page('/politicians/:id', voteRepos.getCRPID, voteRepos.getBioID, voteRepos.index);
 page();
