c12d678c62c545de023513a68a881b5766dab49b
Pat Yolian <pyolian@gmail.com>
2016-04-22 08:45:32 -0700

    rewrite routes and fix conflict in votingController....dejavu?

--- public/scripts/routes.js
+++ public/scripts/routes.js
@@ -1,5 +1,5 @@
 console.log('LOADING ROUTES');
 page('/', search.show, results.compile);
 page('/about', aboutView.index);
-page('/politicians/:id', voteRepos.getCRPID, voteRepos.getBioID, voteRepos.index);
+page('/politicians/:id', Filing.getFilings, voteRepos.getCRPID, Controller.getContributors, voteRepos.getBioID, voteRepos.index);
 page();
