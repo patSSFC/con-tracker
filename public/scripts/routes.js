017015072d6397f62cecfed1b70a715eef8ce0a2
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-04-21 18:08:43 -0700

    udpate about route

--- public/scripts/routes.js
+++ public/scripts/routes.js
@@ -1,6 +1,6 @@
 // page('/', app.initIndexPage);
 console.log('LOADING ROUTES');
 page('/', results.compile);
-page('/about', voteRepos.about);
+page('/about', aboutView.about);
 page('/politicians/:id', voteRepos.getCRPID, voteRepos.getBioID, voteRepos.index);
 page();
