875cfe902d1435329c87cd7692bb007cc1d75d29
Austin Arias <thenandback@gmail.com>
2016-04-24 10:54:00 -0700

    handle potential null errors

--- public/scripts/bioController.js
+++ public/scripts/bioController.js
@@ -3,14 +3,34 @@
 
   var buildBio = function (proto) { // Takes a legislator object and creates a new legislator object with specific properties.
     var bioInfo = {};
-    bioInfo.twitter = 'http://www.twitter.com/' + proto.twitter_id;
-    bioInfo.title = proto.title + '. ' + proto.first_name + ' '+ proto.last_name + ' (' + proto.party + ')';
-    bioInfo.state = proto.state;
-    bioInfo.website = proto.website;
-    bioInfo.phone = proto.phone;
-    bioInfo.party = proto.party;
-    bioInfo.image = 'https://twitter.com/' + proto.twitter_id + '/profile_image?size=original';
+    bioInfo.twitter = 'http://www.twitter.com/' + (proto.twitter_id === undefined ? '' : proto.twitter_id);
+    bioInfo.title = verify(proto.title) + '. ' + verify(proto.first_name) + ' '+ verify(proto.last_name) + ' (' + verify(proto.party) + ')';
+    bioInfo.state = verify(proto.state);
+    bioInfo.website = verify(proto.website);
+    bioInfo.phone = verify(proto.phone);
+    bioInfo.party = verify(proto.party);
+    bioInfo.image = buildImage(verify(proto.twitter_id), verify(proto.party));
     return bioInfo;
+  };
+
+  var verify = function(str) {
+    if (str) {
+      return str;
+    } else {
+      return '';
+    }
+  };
+
+  var buildImage = function(twitter, party) {
+    if (!twitter === 'http://www.twitter.com/') {
+      return 'https://twitter.com/' + twitter + '/profile_image?size=original';
+    } else if (party === 'D') {
+      return 'https://upload.wikimedia.org/wikipedia/commons/6/68/Blue_square_D.PNG';
+    } else if (party === 'R') {
+      return 'http://cf.mp-cdn.net/98/04/2447b67f82daa1ac749162954f08.jpg';
+    } else {
+      return 'http://my.mastersindigitalmarketing.org/dianamaria/wp-content/uploads/sites/1243/2015/10/3.png';
+    }
   };
 
   bio.requestRepos = function (member, callback) {
