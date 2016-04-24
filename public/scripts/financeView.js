db5b8131771c64b96bb3a5124fd738a1b362a924
Pat Yolian <pyolian@gmail.com>
2016-04-24 09:57:41 -0700

    create new chart overtime, remove more comments/logs

--- public/scripts/financeView.js
+++ public/scripts/financeView.js
@@ -4,12 +4,23 @@
   financeViews.chartDataSet = [];
 
   financeViews.createDoughnut = function () {
+    console.log('doughtnut' + myDoughnutChart);
+    var myDoughnutChart;
+    destroyChart();
+    $('#financial-canvas').empty();
     var ctx = $('#financial-canvas').get(0).getContext('2d');
     var data = buildChartData();
 
-    var myDoughnutChart = new Chart(ctx).Doughnut(data,{
+    myDoughnutChart = new Chart(ctx).Doughnut(data,{
       maintainAspectRatio: true
     });
+    financeViews.currentChart = myDoughnutChart;
+  };
+
+  destroyChart = function() {
+    if(financeViews.currentChart !== undefined) {
+      financeViews.currentChart.destroy();
+    }
   };
 
   var buildChartData = function() {
